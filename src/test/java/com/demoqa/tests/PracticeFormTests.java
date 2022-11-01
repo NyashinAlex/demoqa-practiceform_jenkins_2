009315d0d61323138f2a1ad18c693b22f050b397
nyashin alex <Nyashin-AV@ao-rr.rrgit config --global user.email Nyashin-AV@ao-rr.rr>
2022-11-01 17:08:32 +0300

    add new Configuration

--- src/test/java/com/demoqa/tests/PracticeFormTests.java
+++ src/test/java/com/demoqa/tests/PracticeFormTests.java
@@ -36,7 +36,7 @@ public class PracticeFormTests {
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browser_version", "100.0");
         Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
-        if(System.getProperty("remote") != null) {
+        if(System.getProperty("remote") == "true") {
             Configuration.remote = System.getProperty("remote");
         }
     }
